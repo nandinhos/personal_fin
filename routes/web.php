3a00eeb9124907c37ab695a70025b454fb083cc0
Nando Dev <nandinhos@gmail.com>
2026-03-20 04:12:39 -0300

    fix: change root route to return 200 for health check

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use App\Http\Controllers\SubcategoryController;
 use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => redirect()->route('dashboard'));
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard-wrapper');
